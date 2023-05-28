6a9f01f8545cfd664e445eaac7cae54145e8d11f
Lokesh Kumar <neolokeshkr@gmail.com>
2023-05-29 02:07:31 +0530

    Tic Tac Toe game implented without Time Travel Feature

--- src/App.js
+++ src/App.js
@@ -1,32 +1,87 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+function Square({ value, onSquareClick }){
 
-function Square(){
-  return <button className="square">{value}</button>;
+  // const [value, setValue] = useState(null);
 
-  function handleClick(){
+  // function handleClick(){
+  //    setValue('X');
+  // }
 
-  }
+  return <button className="square" onClick={onSquareClick}>{value}</button>;
 }
 
 
 export default function Board() {
+// Creating shared state
+const [squares, setSquares] = useState(Array(9).fill(null));
+const [xIsNext, setXIsNext] = useState(true);
+const [status, setStatus] = useState(null);
+// handleClick has access to squares variable and setSquare function due to closures.
+function handleClick(i){
+  // To check if game's over or avoiding overridng of square 
+  if(squares[i] || calculateWinner(squares)){
+    return;
+  }
+
+  // Logic for state updation
+  const nextSquares = squares.slice();
+  if(xIsNext){
+    nextSquares[i]='X'
+  } else {
+    nextSquares[i]='O'
+  }
+  setSquares(nextSquares);
+  setXIsNext(!xIsNext)
+
+
+  const winner = calculateWinner(squares);
+  if (winner) {
+    setStatus("Winner: " + winner)
+  } else {
+    setStatus("Next player: " + (xIsNext ? "X" : "O"));
+  }
+
+}
+// console.log(squares);
+
   return (
     <React.Fragment>
+      <div className="status">{status}</div>
       <div className='board-row'>
-        <Square value='X'/>
-        <Square value='X'/>
-        <Square value='X'/>
+        <Square value={squares[0]} onSquareClick={() => {handleClick(0)}} />
+        <Square value={squares[1]} onSquareClick={() => {handleClick(1)}}/>
+        <Square value={squares[2]} onSquareClick={() => {handleClick(2)}}/>
       </div>
       <div className='board-row'>
-        <Square value='X'/>
-        <Square value='X'/>
-        <Square value='X'/>
+        <Square value={squares[3]} onSquareClick={() => {handleClick(3)}}/>
+        <Square value={squares[4]} onSquareClick={() => {handleClick(4)}}/>
+        <Square value={squares[5]} onSquareClick={() => {handleClick(5)}}/>
       </div>
       <div className='board-row'>
-        <Square value='X'/>
-        <Square value='X'/>
-        <Square value='X'/>
+        <Square value={squares[6]} onSquareClick={() => {handleClick(6)}}/>
+        <Square value={squares[7]} onSquareClick={() => {handleClick(7)}}/>
+        <Square value={squares[8]} onSquareClick={() => {handleClick(8)}}/>
       </div>
     </React.Fragment>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
